6d41df155e278c0c275df5271e7effc4af2bf087
Prawee Wongsa <prawee@hotmail.com>
2015-03-20 07:57:08 +0700

    Initial commit.

---
+++ client/templates/home.js
@@ -0,0 +1,21 @@
+Template.home.created=function(){
+	console.log('Created the home template');
+}
+
+Template.home.rendered=function(){
+	console.log('Rendered the home template');
+}
+
+Template.home.destroyed=function(){
+	console.log('Destroyed the home template');
+}
+
+Template.home.helpers({
+	/*exampleHelper:function(){
+		return 'This text came from a helper with some <strong>HTML</strong>.';
+	}*/
+
+	podHelper:function(){
+		return new Spacebars.SafeString('This text came from a helper with some <strong>HTML</strong>.');
+	}
+});
