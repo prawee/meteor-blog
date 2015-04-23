9f68a50b18c815525b1a8f6b9c7ec7015d83a48d
Prawee Wongsa <prawee@hotmail.com>
2015-04-24 01:25:51 +0700

    getting lazyload-posts with limit session.

--- client/subscriptions.js
+++ client/subscriptions.js
@@ -1,3 +1,8 @@
 //Meteor.subscribe('all-posts');
-Meteor.subscribe('limited-posts');
+//Meteor.subscribe('limited-posts');
 //Meteor.subscribe('specificfields-posts');
+
+Session.setDefault('lazyloadLimit',2);
+Tracker.autorun(function(){
+    Meteor.subscribe('lazyload-posts',Session.get('lazyloadLimit'));
+});
