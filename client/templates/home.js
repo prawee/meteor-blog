8cfa808daa1b5a43bbc6f47f33dca51686493010
Prawee Wongsa <prawee@hotmail.com>
2015-04-24 01:31:29 +0700

    adding click event to load lazyload-limit with session to home template.

--- client/templates/home.js
+++ client/templates/home.js
@@ -45,3 +45,10 @@ Template.home.helpers({
         return Posts.find({},{sort:{timeCreated:-1}});
     }
 });
+
+Template.home.events({
+    'click button.lazyload':function(e,template){
+        var currentLimit=Session.get('lazyloadLimit');
+        Session.set('lazyloadLimit',currentLimit+2);
+    }
+});
