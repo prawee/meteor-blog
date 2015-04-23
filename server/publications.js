14197ffb70a37b643bccb69249555fee9471fb48
Prawee Wongsa <prawee@hotmail.com>
2015-04-24 00:17:06 +0700

    publishing only parts of data.

--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
+/* all
 Meteor.publish('all-posts',function(){
     return Posts.find();
+});
+*/
+
+Meteor.publish('limited-posts',function(){
+    return Posts.find({},{
+        limit:2,
+        sort:{timeCreated:-1}
+    });
 });
