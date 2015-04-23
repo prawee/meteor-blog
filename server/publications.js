af4ed31097e5125897a0c45dfe726ac33f2b239e
Prawee Wongsa <prawee@hotmail.com>
2015-04-24 01:06:15 +0700

    adding lazyload-posts to publications.js

--- server/publications.js
+++ server/publications.js
@@ -4,16 +4,16 @@ Meteor.publish('all-posts',function(){
 });
 */
 
-//limit
+/*limit
 Meteor.publish('limited-posts',function(){
     return Posts.find({},{
         limit:2,
         sort:{timeCreated:-1}
     });
 });
+*/
 
-
-/*
+/* spectificfields
 Meteor.publish('specificfields-posts',function(){
     return Posts.find({},{
         fields:{
@@ -22,3 +22,13 @@ Meteor.publish('specificfields-posts',function(){
     });
 });
 */
+
+Meteor.publish('lazyload-posts',function(limit){
+    return Posts.find({},{
+        limit:limit,
+        fields:{
+            text:0
+        },
+        sort:{timeCreated:-1}
+    });
+});
